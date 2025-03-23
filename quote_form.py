fabd829eb2ff4c66ead34c52ab16538d0dc28917
kazuya2400364 <kazuya2400364@gmail.com>
2025-03-23 16:29:50 +0900

    first commit

---
+++ quote_form.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import requests
+
+def get_address(zip_code):
+  res = requests.get("https://zipcloud.ibsnet.co.jp/api/search",
+                   params={"zipcode":str(zip_code)})
+  data = res.json()["results"][0]
+  address = data["address1"] + data["address2"] + data["address3"]
+  return address
+  
+def main():
+  st.title("見積フォーム")
+  st.subheader("お客様情報を入力する")
+  zip_code = st.text_input("郵便番号", placeholder = "1234567")
+  address = st.text_input("住所", value = get_address(zip_code) if zip_code and len(zip_code) == 7 and zip_code.isdigit() else "", placeholder = "東京都港区海岸1-5-20")
+  name = st.text_input("お名前", placeholder = "東京太郎")
+  phone_number = st.text_input("電話番号", placeholder = "0123456789")
+  emali = st.text_input("メールアドレス")
+
+  if st.button("登録する"):
+    if not address or not name or not phone_number or not emali:
+      st.error("すべての項目を入力してください。")
+    else:
+      st.subheader("おすすめの製品")
+      st.image("model.png")
+      st.text("型番：S224ATCS-W(6畳用)")
+      st.text("機器販売価格：102,170円")
+      st.text("基本工事費：35,530円")
+      st.text("補助金額：30,000pt")
+      st.button("本見積もりはこちらから")
+
+if __name__ == '__main__':
+  main()
